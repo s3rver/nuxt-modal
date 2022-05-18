f5233648a0934ff48235a2996207f90fce1ec966
Alireza Fakharzadeh <mr.s3rver@gmail.com>
2022-05-18 06:23:05 -0700

    fixed mobile layout

--- template/layouts/defaultMobile.vue
+++ template/layouts/defaultMobile.vue
@@ -13,11 +13,34 @@
             <div
                 v-if="is_show"
                 :style="{
-                    'z-index': zIndex,
+                  'z-index': zIndex,
                 }"
-                class="fixed box-modal inset-x-0 bottom-0 mx-auto p-5 py-3 rounded-t-3xl overflow-x-hidden"
+                class="bg-white fixed inset-x-0 bottom-0 mx-auto p-5 py-3 max-h-[96vh] rounded-t-3xl overflow-x-hidden"
             >
-                <div class="h-1.5 mx-auto w-24 bg-gray-400 rounded-full mb-3"></div>
+                <div
+                    v-if="modal.title"
+                    class="flex justify-between items-center border-b border-gray-200 pb-2 mb-1"
+                >
+                    <h2 class="text-xl">{{ modal.title }}</h2>
+
+                    <svg
+                        v-if="!modal.disabled"
+                        class="w-5 h-5 cursor-pointer"
+                        viewBox="0 0 50 45"
+                        xmlns="http://www.w3.org/2000/svg"
+                        @click="close"
+                    >
+                        <path
+                            d="M9.15625 6.3125L6.3125 9.15625L22.15625 25L6.21875 40.96875L9.03125 43.78125L25 27.84375L40.9375 43.78125L43.78125 40.9375L27.84375 25L43.6875 9.15625L40.84375 6.3125L25 22.15625Z"
+                            fill="#ee0000"
+                        />
+                    </svg>
+                </div>
+
+                <div
+                    v-else
+                    class="h-1.5 mx-auto w-24 bg-gray-200 rounded-full mb-3"
+                ></div>
 
                 <slot />
             </div>
@@ -34,10 +57,6 @@ export default {
 </script>
 
 <style scoped>
-.box-modal {
-    background: red;
-}
-
 .modal-fade-enter-active,
 .modal-fade-leave-active {
     transition: opacity 0.2s;
